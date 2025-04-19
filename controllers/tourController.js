7251d5d69c7c498f11b39c00c918039819cb9eb2
BothSann <boththann76@gmail.com>
2025-04-19 21:09:19 +0700

    feat: add getTourStats function to aggregate tour statistics

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -118,3 +118,39 @@ export const deleteTour = async (request, response) => {
     });
   }
 };
+
+export const getTourStats = async (request, response) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: "$difficulty" },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: "$ratingsQuantity" },
+          avgRating: { $avg: "$ratingsAverage" },
+          avgPrice: { $avg: "$price" },
+          minPrice: { $min: "$price" },
+          maxPrice: { $max: "$price" },
+        },
+      },
+      {
+        $sort: { numRatings: 1 },
+      },
+    ]);
+
+    response.status(200).json({
+      status: "success",
+      data: {
+        stats,
+      },
+    });
+  } catch (err) {
+    response.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
