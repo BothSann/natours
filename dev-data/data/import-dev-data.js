19ed708383e3e4822d41a44411280cd9a50aa510
BothSann <boththann76@gmail.com>
2025-04-19 13:50:37 +0700

    feat: add data import and deletion functionality for tours

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,58 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import fs from "fs";
+import Tour from "./../../models/tourModel.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+dotenv.config({ path: "./config.env" });
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connection successful!");
+  });
+
+// Read JSON file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, "utf-8")
+);
+
+// Import data into DB
+
+const importData = async () => {};
+try {
+  await Tour.create(tours);
+  console.log("Data successfully loaded!");
+  process.exit();
+} catch (err) {
+  console.log(err);
+}
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log("Data successfully deleted!");
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+if (process.argv[2] === "--import") {
+  importData();
+} else if (process.argv[2] === "--delete") {
+  deleteData();
+}
