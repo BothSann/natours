0ca87b68b4cf4804ae94ce9818f021edcc2fe43d
BothSann <boththann76@gmail.com>
2025-04-19 21:19:40 +0700

    feat: add getMonthlyPlan function to aggregate tour starts by month

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -154,3 +154,74 @@ export const getTourStats = async (request, response) => {
     });
   }
 };
+
+export const getMonthlyPlan = async (request, response) => {
+  try {
+    const year = request.params.year * 1; // Convert string to number
+    const plan = await Tour.aggregate([
+      {
+        $unwind: "$startDates",
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: "$startDates" },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: "$name" },
+        },
+      },
+      {
+        $addFields: {
+          month: "$_id",
+          monthName: {
+            $arrayElemAt: [
+              [
+                "",
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December",
+              ],
+              "$_id",
+            ],
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: { numTourStarts: -1 },
+      },
+    ]);
+
+    response.status(200).json({
+      status: "success",
+      data: {
+        plan,
+      },
+    });
+  } catch (err) {
+    response.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
