0b655e779ef79f094c4bf0fa7c73054d3f5f63dc
BothSann <boththann76@gmail.com>
2025-04-19 17:31:03 +0700

    refactor: import APIQueryBuilder and remove its inline definition for cleaner code structure

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 import Tour from "./../models/tourModel.js";
+import APIQueryBuilder from "./../utils/apiQueryBuilder.js";
 
 export const aliasTopTours = (request, response, next) => {
   request.query.limit = "5";
@@ -6,61 +7,6 @@ export const aliasTopTours = (request, response, next) => {
   request.query.fields = "name,price,ratingsAverage,summary,difficulty";
   next();
 };
-
-class APIQueryBuilder {
-  constructor(mongooseQuery, requestQuery) {
-    this.mongooseQuery = mongooseQuery;
-    this.requestQuery = requestQuery;
-  }
-
-  filter() {
-    // Build Query
-    /// 1) Filtering
-    const queryObj = { ...this.requestQuery };
-    const excludedFields = ["page", "sort", "limit", "fields"];
-    excludedFields.forEach((el) => delete queryObj[el]);
-    console.log(queryObj);
-
-    // 2) Advanced Filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    this.mongooseQuery = this.mongooseQuery.find(JSON.parse(queryStr));
-
-    return this;
-  }
-
-  sort() {
-    if (this.requestQuery.sort) {
-      const sortBy = this.requestQuery.sort.split(",").join(" ");
-      this.mongooseQuery = this.mongooseQuery.sort(sortBy);
-    } else {
-      this.mongooseQuery = this.mongooseQuery.sort("-createdAt");
-    }
-
-    return this;
-  }
-
-  limitFields() {
-    if (this.requestQuery.fields) {
-      const fields = this.requestQuery.fields.split(",").join(" ");
-      this.mongooseQuery = this.mongooseQuery.select(fields);
-    } else {
-      this.mongooseQuery = this.mongooseQuery.select("-__v");
-    }
-
-    return this;
-  }
-
-  paginate() {
-    const page = this.requestQuery.page * 1 || 1;
-    const limit = this.requestQuery.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-    this.mongooseQuery = this.mongooseQuery.skip(skip).limit(limit);
-
-    return this;
-  }
-}
 
 export const getAllTours = async (request, response) => {
   try {
