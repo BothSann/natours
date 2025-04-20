522f21254de2aedfe5a0e9bbdae3cec4bbb5ce25
BothSann <boththann76@gmail.com>
2025-04-20 15:40:25 +0700

    feat: refactor error handling middleware to use AppError for centralized error responses

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from "express";
 import morgan from "morgan";
 import tourRouter from "./routes/tourRoutes.js";
 import userRouter from "./routes/userRoutes.js";
+import { AppError } from "./utils/appError.js";
+import globalErrorHandler from "./controllers/errorController.js";
 
 const app = express();
 
@@ -13,11 +15,14 @@ app.use("/api/v1/users", userRouter);
 
 // Error handling middleware
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "fail",
-    message: `Can't find ${req.originalUrl} on this server!`,
-  });
-  next();
+  const err = new AppError(
+    `Can't find ${req.originalUrl} on this server!`,
+    404
+  );
+
+  next(err);
 });
+
+app.use(globalErrorHandler);
 
 export default app;
