75b23b2484e6af5f9c0a43d7aab4a71d320e67b6
BothSann <boththann76@gmail.com>
2025-04-20 14:52:10 +0700

    feat: add error handling middleware for 404 responses

--- app.js
+++ app.js
@@ -11,4 +11,13 @@ app.use(express.json());
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
+// Error handling middleware
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+  next();
+});
+
 export default app;
