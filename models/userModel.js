cb4599e617a4d3abea45ac7851afdf9cc9dc4fae
BothSann <boththann76@gmail.com>
2025-04-22 12:03:10 +0700

    feat: implement user model with password hashing and validation

---
+++ models/userModel.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please tell us your name!"],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide your email!"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Please provide a valid email!"],
+  },
+  photo: {
+    type: String,
+    default: "default.jpg",
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password!"],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password!"],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Passwords do not match!",
+    },
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
