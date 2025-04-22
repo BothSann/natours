b865cb01d3947d2cbc44fb0c80c7634e129d03a9
BothSann <boththann76@gmail.com>
2025-04-22 14:43:18 +0700

    feat: implement user authentication with token generation and password validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,17 @@
-export const getAllUsers = (request, response) => {
-  response.status(500).json({
-    status: "error",
-    message: "This route is not yet defined!",
+import catchAsync from "./../utils/catchAsync.js";
+import User from "./../models/userModel.js";
+
+export const getAllUsers = catchAsync(async (request, response, next) => {
+  const allUsers = await User.find();
+
+  response.status(200).json({
+    status: "success",
+    length: allUsers.length,
+    data: {
+      users: allUsers,
+    },
   });
-};
+});
 
 export const getUserById = (request, response) => {
   response.status(500).json({
