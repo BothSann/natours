4d5bbc99da391a00f56760d2331118a1ecdcdf46
BothSann <boththann76@gmail.com>
2025-04-23 13:58:42 +0700

    feat: add user role to user model and implement role-based access control in tour routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -67,7 +67,6 @@ export const protect = catchAsync(async (req, res, next) => {
     req.headers.authorization.startsWith("Bearer")
   ) {
     token = req.headers.authorization.split(" ")[1];
-    console.log(req.user);
   }
 
   if (!token)
@@ -97,3 +96,15 @@ export const protect = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+
+export const restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError("You do not have permission to perform this action!", 403)
+      );
+    }
+
+    next();
+  };
+};
