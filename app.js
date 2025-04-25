d4e477d740dd83ed33969090de4b6303c70decef
BothSann <boththann76@gmail.com>
2025-04-25 13:02:46 +0700

    feat: implement rate limiting middleware to control request frequency

--- app.js
+++ app.js
@@ -4,8 +4,18 @@ import tourRouter from "./routes/tourRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import { AppError } from "./utils/appError.js";
 import globalErrorHandler from "./controllers/errorController.js";
+import { rateLimit } from "express-rate-limit";
 
 const app = express();
+
+const limiter = rateLimit({
+  max: 100, // 100 request from the same IP
+  windowMs: 60 * 60 * 1000, // 1hour
+  message: "Too many requests, please try again later.",
+  legacyHeaders: true,
+});
+
+app.use("/api", limiter);
 
 // Middleware
 app.use(morgan("dev"));
