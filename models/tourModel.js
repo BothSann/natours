4a1c9c6248418079c6b86ed364d8edd23b6d6c62
BothSann <boththann76@gmail.com>
2025-04-26 17:23:19 +0700

    feat: add query performance logging and populate guides field in tour schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -166,11 +166,22 @@ tourSchema.pre("save", function (next) {
 tourSchema.pre(/^find/, function (next) {
   this.find({ secretTour: { $ne: true } });
   this.start = Date.now();
+
   next();
 });
 
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+
+  next();
+});
+
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "guides",
+    select: "-__v -passwordChangedAt",
+  });
+
   next();
 });
 
