a3392d4583dca72bb4a41199215b5e19350e6bc8
BothSann <boththann76@gmail.com>
2025-04-28 15:19:59 +0700

    feat: add createOne and updateOne controller functions for document management

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,6 +1,18 @@
 import catchAsync from "../utils/catchAsync.js";
 import { AppError } from "../utils/appError.js";
 
+export const createOne = (Model) =>
+  catchAsync(async (request, response, next) => {
+    const newDoc = await Model.create(request.body);
+
+    response.status(201).json({
+      status: "success",
+      data: {
+        data: newDoc,
+      },
+    });
+  });
+
 export const deleteOne = (Model) =>
   catchAsync(async (request, response, next) => {
     const doc = await Model.findByIdAndDelete(request.params.id);
@@ -12,5 +24,28 @@ export const deleteOne = (Model) =>
     response.status(204).json({
       status: "success",
       data: null,
+    });
+  });
+
+export const updateOne = (Model) =>
+  catchAsync(async (request, response, next) => {
+    const updateDoc = await Model.findByIdAndUpdate(
+      request.params.id,
+      request.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    if (!updateDoc) {
+      return next(new AppError("No doc found with that ID", 404));
+    }
+
+    response.status(200).json({
+      status: "success",
+      data: {
+        data: updateDoc,
+      },
     });
   });
