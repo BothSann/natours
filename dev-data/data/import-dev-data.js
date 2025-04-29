f5e8ed71cf7d6d5927a56882cec070e8681dcb65
BothSann <boththann76@gmail.com>
2025-04-29 16:17:00 +0700

    feat: enhance import-dev-data script to include user and review data import/export

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import fs from "fs";
 import Tour from "./../../models/tourModel.js";
+import User from "./../../models/userModel.js";
+import Review from "./../../models/reviewModel.js";
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -27,12 +29,18 @@ mongoose
 
 // Read JSON file
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, "utf-8"));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, "utf-8"));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, "utf-8")
+);
 
 // Import data into DB
 
 const importData = async () => {};
 try {
   await Tour.create(tours);
+  await User.create(users, { validateBeforeSave: false });
+  await Review.create(reviews);
   console.log("Data successfully loaded!");
   process.exit();
 } catch (err) {
@@ -42,6 +50,8 @@ try {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log("Data successfully deleted!");
     process.exit();
   } catch (err) {
